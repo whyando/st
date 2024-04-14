ec82133207ec3e89bda19a07504564369e839098
Justin Auger <justinauger@gmail.com>
2024-04-14 11:15:39 +0100

    debug view tweaks + re-enable extra 25 explorers.

--- src/bin/reachable_systems.rs
+++ src/bin/reachable_systems.rs
@@ -3,6 +3,7 @@ use st::api_client::ApiClient;
 use st::db::DbClient;
 use st::universe::pathfinding::EdgeType;
 use st::universe::Universe;
+use std::cmp::min;
 use std::env;
 use std::fs::File;
 use std::io;
@@ -58,7 +59,7 @@ async fn main() -> io::Result<()> {
         Some((cd, _path)) => *cd,
         None => &i64::MAX,
     });
-    starter_systems = starter_systems.into_iter().take(100).collect();
+    // starter_systems = starter_systems.into_iter().take(100).collect();
 
     // output to ./starter_systems.txt
     let mut f = File::create("starter_systems.txt")?;
@@ -74,6 +75,11 @@ async fn main() -> io::Result<()> {
                     "{}. {}: {}h {}m {}s",
                     idx, system, cd_hours, cd_minutes, cd_seconds
                 )?;
+                const MAX_FUEL: i64 = 800;
+                const MAX_CARGO_FUEL: i64 = 40;
+                let mut fuel: i64 = MAX_FUEL;
+                let mut cargo_fuel: i64 = MAX_CARGO_FUEL;
+                let mut out_of_fuel: bool = false;
                 for pair in path.windows(2) {
                     let s = &pair[0];
                     let t = &pair[1];
@@ -82,15 +88,44 @@ async fn main() -> io::Result<()> {
                         EdgeType::Warp => "W",
                         EdgeType::Jumpgate => "J",
                     };
+                    match edge.edge_type {
+                        EdgeType::Warp => {
+                            // refuel to at least edge.fuel
+                            let refuel = {
+                                let missing_fuel = MAX_FUEL - fuel;
+                                // round down to the nearest 100, so we don't buy more than we need
+                                let units = (missing_fuel / 100) * 100;
+                                if units + fuel < edge.fuel {
+                                    missing_fuel
+                                } else {
+                                    units
+                                }
+                            };
+                            let refuel = min(refuel, cargo_fuel * 100);
+                            let fuel_cargo_units = (refuel + 99) / 100;
+                            fuel += refuel - edge.fuel;
+                            cargo_fuel -= fuel_cargo_units;
+                            if fuel < 0 || cargo_fuel < 0 {
+                                out_of_fuel = true;
+                            }
+                        }
+                        EdgeType::Jumpgate => {
+                            fuel = MAX_FUEL;
+                            cargo_fuel = MAX_CARGO_FUEL;
+                        }
+                    }
                     writeln!(
                         &mut f,
                         "\t{} {} {}f {}s",
                         type_, t, edge.fuel, edge.duration
                     )?
                 }
+                if out_of_fuel {
+                    writeln!(&mut f, "\tOUT OF FUEL")?;
+                }
             }
             None => {
-                writeln!(&mut f, "{}: unreachable", system)?;
+                writeln!(&mut f, "{}. {}: unreachable", idx, system)?;
             }
         }
     }
